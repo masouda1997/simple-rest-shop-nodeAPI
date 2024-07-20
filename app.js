d8f060bba23a245ec9dbb0c5b4fe8bfc502659bf
masouda1997 <masoud30376@gmail.com>
2024-07-20 17:03:16 +0330

    add base files

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express();
+
+app.use((req, res, next)=>{
+   res.status(200).json({"msg":"works"})
+})
+
+
+module.exports = app
---
+++ server.js
@@ -0,0 +1,8 @@
+const http = require('http')
+const  app= require('./app')
+
+const port = process.env.PORT || 8001
+
+const server = http.createServer(app)
+
+server.listen(port)
