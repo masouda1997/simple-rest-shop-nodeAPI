a37719ed82cc7a35a43986eb4e1d2fd7bd770f29
masouda1997 <masoud30376@gmail.com>
2024-07-22 11:26:54 +0330

    error handling

--- app.js
+++ app.js
@@ -5,8 +5,25 @@ const ordersRoutes = require('./api/routes/orders')
 
 const app = express();
 
-app.use('/',(req,res) => {res.status(200).send('api route contain these routes : \n /products \n /orders')})
+// app.use('/',(req,res) => {res.status(200).send('api route contain these routes : \n /products \n /orders')})
 app.use('/products' , productRoutes)
 app.use('/orders' , ordersRoutes)
+
+app.use((req,res,next)=>{
+   const error = new Error('Not found')
+   // we can use the comment code to show the automatic error handler works 
+   // error.status(404)
+   error.status = 404
+   next(error)
+})
+// for the time that custom errors nowt handle 
+app.use((error ,req,res,next)=> {
+   res.status(error.status || 500)
+   res.json({
+      error:{
+         msg:error.message
+      }
+   })
+})
 
 module.exports = app
--- server.js
+++ server.js
@@ -15,6 +15,22 @@ shop.use(express.urlencoded({extended:false}))
 
 shop.use(morgan('dev'))
 shop.use('/api' , app)
+
+
+app.use((req,res,next)=>{
+   const error = new Error('Not found')
+   error.status(404)
+   next(error)
+})
+// for the time that custom errors nowt handle 
+app.use((error ,req,res,next)=> {
+   res.status(error.status || 500)
+   res.json({
+      error:{
+         msg:error.message
+      }
+   })
+})
 shop.listen(port, () => console.log(`Shop.api app listening on port ${port}!`))
 
 
